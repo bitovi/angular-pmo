4cfd3be476f559b186b33d665356c69f6202b737
Wade H <wade@bitovi.com>
2022-04-20 15:43:34 -0400

    chore: upgrade venerabilities and add NgRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { OrderListComponent } from './order/list/list.component';
 import { OrderHistoryComponent } from './order/history/history.component';
 import { OnlyNumbersDirective } from './shared/only-numbers.directive';
 import { ItemTotalPipe } from './shared/item-total.pipe';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { ItemTotalPipe } from './shared/item-total.pipe';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    StoreModule.forRoot({}, {})
   ],
   providers: [
     ItemTotalPipe,
